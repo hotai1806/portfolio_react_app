640d6a6b8c8cea092619ca094398cf6cae398203
unknown <hotai1806@gmail.com>
2020-06-24 21:30:17 +0700

    frist_version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 import './customs.css';
-
+import {SimpleSlide} from './components/';
 
 // const useStyles = makeStyles((theme) => ({
   
@@ -21,25 +21,85 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <div className="sumary" >I strive and work hard to succeed.Optimistic for life. Don't be sorry. Be better</div> 
       </header>
       <div className="big">
+       
          <div className="div-avatar">
         <img src={require('./images/profile.jpg')} className="avatar" alt='' />
       </div>
+  
+      
+
       <div className="out-box" >
+      
           <div className="box">
+           
             <div className="right top" >  </div>
-                <div className="paper"> Skill</div>
+                <div className="paper"> <u>Project</u>
+                <div>Buid online shop with Django </div>
+                <ul >
+                  <li> Build a web store to sell mobile phone</li>
+                  <li>Store data in database </li>
+                  <li> Technologies used : HTML, CSS, Python, Django, SQLite </li>
+                  <li>Github: <a href="https://github.com/hotai1806/online_shoping_django"></a>https://github.com/hotai1806/online_shoping_django  </li>
+                  <li>Demo: <a href="https://herokudjangoapp1999.herokuapp.com/">link</a> </li>
+                </ul>
+                <div>Catch chicken eggs game</div>
+                <ul >
+                  <li> Make game catching eggs</li>
+                  <li> Technologies used : Pyglet, Python </li>
+                  <li>Github: <a href="https://github.com/hotai1806/catch_chicken_eggs"></a>https://github.com/hotai1806/catch_chicken_eggs  </li>
+                  
+                </ul>
+                
+                </div>
             </div>
+            <SimpleSlide  />
             <div className="box">
+           
                 <div className="left top" >  </div>
-            <div className="paper"> Skill</div>
+            <div className="paper"> Skill
+            <div >PROFICENT</div>
+                <ul >
+                  <li>Python</li>
+                  
+                  
+                </ul>
+           
+            <div >FAMLILAR</div>
+                <ul >
+                  <li>C#, C++</li>
+                  
+                  
+                </ul>
+           
+            <div >FREAMEWORK-LIBRARY</div>
+                <ul >
+                  <li>Django</li>
+                  
+                  
+                </ul>
+            
+            <div >DATABASE</div>
+                <ul >
+                  <li>MYSQL, SQLite, SQLSERVER</li>
+                  
+                  
+                </ul>
+                <div >OTHER</div>
+                <ul >
+                  <li>HTML5, CSS3, GIT, LINUX</li>
+                  
+                  
+                </ul>
+            </div>
             </div>
           </div>
       </div>
-        
+        <footer> <div className="footer"></div></footer>
       </div>
+      
    
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import Paper from '@material-ui/core/Paper';
+import Slide from '@material-ui/core/Slide';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import ContactsIcon from '@material-ui/icons/Contacts';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+import HomeIcon from '@material-ui/icons/Home';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: 180,
+    position:'absolute',
+
+    right: "2%",
+    zIndex:1
+  },
+  wrapper: {
+    width: 100 + theme.spacing(2),
+  },
+  paper: {
+    zIndex: 1,
+    padding: theme.spacing(2),
+    margin: theme.spacing(1),
+    float: 'right',
+    width:'200px',
+    textAlign:'left',
+  },
+  svg: {
+    width: 100,
+    height: 100,
+  },
+  polygon: {
+    fill: theme.palette.common.white,
+    stroke: theme.palette.divider,
+    strokeWidth: 1,
+  },
+}));
+
+export const SimpleSlide =() => {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+      
+      <Button style={{  height: '40px', width:'40px', }}  variant="contained" color="primary" checked={checked} onClick={handleChange} label="Show" />
+
+        <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
+          <Paper  elevation={5} className={classes.paper}>
+          <div style={{padding:'5px'}} > <ContactsIcon />  0907779873</div>
+          <div style={{padding:'5px'}} > <GitHubIcon/> <a href='https://github.com/hotai1806'>hotai1806</a> </div>
+          
+          <div style={{padding:'5px'}} > <EmailIcon/> hotai1806@gmail.com</div>
+          <div style={{padding:'5px'}} > <HomeIcon/> TP.HCM</div>
+          </Paper>
+        </Slide>
+      </div>
+    </div>
+  );
+}
